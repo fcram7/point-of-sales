a18fe7ed15e339a0f92da756808108029c87c22c
fachri achmad maulana <fachri57@yahoo.com>
2024-09-08 15:20:32 +0700

    modified navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,14 @@ import { signOut } from '@/api/auth';
 import { useRouter } from 'next/navigation';
 import { useToast } from './ui/use-toast';
 import { ToastAction } from './ui/toast';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from './ui/navigation-menu';
 
 export const Navbar = () => {
   const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
@@ -32,7 +40,7 @@ export const Navbar = () => {
     return () => authListener.subscription.unsubscribe();
   }, []);
 
-  const onLogoutHandler = async (e: MouseEvent<HTMLButtonElement>) => {
+  const onLogoutHandler = async (e: MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     e.stopPropagation();
 
@@ -49,17 +57,44 @@ export const Navbar = () => {
       console.log(err);
     }
   };
+
   return (
     <header className='navbar-section fixed w-full px-[4%] lg:px-[2%] bg-transparent backdrop-blur-sm z-10 bg-slate-200'>
-      <nav className='navbar-content py-4 flex items-center justify-between'>
-        <div className='navbar-logo text-2xl text-slate-900'>
+      <nav className='navbar-section__content py-4 flex items-center justify-between'>
+        <div className='navbar-section__logo text-2xl text-slate-900'>
           <h1>SimplePOS</h1>
         </div>
 
         {loggedInUser ? (
-          <Button type='button' onClick={onLogoutHandler}>
-            Logout
-          </Button>
+          <>
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>
+                    {loggedInUser.email === 'superadmin@simplepos.com'
+                      ? 'Welcome, superadmin'
+                      : null}
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <div className='navbar-section__navmenu-content w-48'>
+                      <ul className='navbar-section__navmenu-link-container px-4 py-2 grid gap-4'>
+                        <li>
+                          <NavigationMenuLink asChild>
+                            <div className='cursor-pointer xl:text-md'>All Menus</div>
+                          </NavigationMenuLink>
+                        </li>
+                        <li>
+                          <NavigationMenuLink asChild onClick={onLogoutHandler}>
+                            <div className='cursor-pointer xl:text-md'>Logout</div>
+                          </NavigationMenuLink>
+                        </li>
+                      </ul>
+                    </div>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </>
         ) : (
           <Button disabled>No Login Detected</Button>
         )}
