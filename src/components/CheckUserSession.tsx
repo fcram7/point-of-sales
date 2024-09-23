0bad52e8dc9abb8161bc8605e7580abbcf82ecd6
fachri achmad maulana <fachri57@yahoo.com>
2024-09-23 20:15:00 +0700

    minor changes

--- src/components/CheckUserSession.tsx
+++ src/components/CheckUserSession.tsx
@@ -1,8 +1,6 @@
 import { getUserSession } from '@/api/auth';
 import { useRouter } from 'next/navigation';
-import { Children, ReactNode, useEffect } from 'react';
-import { useToast } from './ui/use-toast';
-import { ToastAction } from './ui/toast';
+import { ReactNode, useEffect } from 'react';
 
 interface checkUserSession {
   routeTo: string;
@@ -11,8 +9,8 @@ interface checkUserSession {
 
 export const CheckUserSession = ({ routeTo, children }: checkUserSession) => {
   const router = useRouter();
-  // const { toast } = useToast();
 
+  //user session check
   useEffect(() => {
     const checkUserSession = async () => {
       const { data } = await getUserSession();
@@ -21,13 +19,6 @@ export const CheckUserSession = ({ routeTo, children }: checkUserSession) => {
         router.push(routeTo);
       } else {
         router.push('/');
-
-        // return toast({
-        //   title: 'No session detected',
-        //   action: (
-        //     <ToastAction altText='Click to close notification'>Close</ToastAction>
-        //   )
-        // })
       }
     };
 
